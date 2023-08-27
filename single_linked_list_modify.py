b6a1ae266dcff4f6a5b3e67a3502fcd8d05b1f87
memoryT <cn64history@zohomail.com>
2023-08-27 15:54:42 +0800

    2_error_when delete all data

--- single_linked_list_modify.py
+++ single_linked_list_modify.py
@@ -30,7 +30,19 @@ def insertnode(head, ptr, num, salary, name):
         ptr.next = InsertNode
     return head
 
-position = 0
+def del_ptr(head, ptr):
+    top = head
+    if ptr.num == head.num:
+        head = head.next
+    else:
+        while top.next != ptr:
+            top = top.next
+        if ptr.next == None:
+            top.next = None
+        else:
+            top.next = ptr.next
+    return head
+
 data = [[1001, 32367], [1002, 24388], [1003, 27556], [1007, 31299],
         [1012, 42660], [1014, 25676], [1018, 44145], [1043, 52182],
         [1031, 32769], [1037, 21100], [1041, 32196], [1046, 25776]]
@@ -59,18 +71,42 @@ for i in range(1,12):
     ptr.next = newnode
     ptr = ptr.next
 
+position = 0
 while True:
-    print("請輸入要插入的員工編號，若編號不存在則放在串列首，輸入-1退出")
-    position = int(input())
-    if position == -1:
+    print("""請選擇模式：
+    I:插入；D：刪除；E：退出""")
+    operation = input()
+    if operation == "E":
         break
-    else:
-        ptr = findnode(head, position)
-        new_num = int(input("員工編號："))
-        new_salary = int(input("薪水："))
-        new_name = input("員工姓名：")
-        head = insertnode(head, ptr, new_num, new_salary, new_name)
-    print()
+    elif operation == "I":
+        while True:
+            print("請輸入要插入的員工編號，若編號不存在則放在串列首，輸入-1退出插入模式")
+            position = int(input())
+            if position == -1:
+                break
+            else:
+                ptr = findnode(head, position)
+                new_num = int(input("員工編號："))
+                new_salary = int(input("薪水："))
+                new_name = input("員工姓名：")
+                head = insertnode(head, ptr, new_num, new_salary, new_name)
+    elif operation == "D":
+        while True:
+            print("請輸入要刪除的員工編號，輸入-1退出刪除模式")
+            position = int(input())
+            if position == -1:
+                break
+            else:
+                ptr = head
+                count = -1
+                while ptr != None:
+                    if ptr.num == position:
+                        ptr = del_ptr(head, ptr)
+                        count += 1
+                        head = ptr
+                    ptr = ptr.next
+                if count == -1:
+                    print("無該員工編號")
 
 ptr = head
 while ptr != None:
